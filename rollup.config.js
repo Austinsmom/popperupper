bf42e5c7458374303ad14513edef1766de20f600
Sam Gaus <sam@gaus.co.uk>
2017-06-16 16:01:18 +0100

    v1.0.3 More publishing issues

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import cssnext from 'postcss-cssnext';
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const external = Object.keys(pkg.dependencies || {});
 
 export default [
   {
@@ -19,9 +23,8 @@ export default [
   {
     entry: 'src/index.js',
     format: 'cjs',
+    external,
     plugins: [
-      resolve(),
-      commonjs({ namedExports: { 'iframe-resizer': ['iframeResizer'] } }),
       babel({ include: 'src/**/*.js' }),
       postcss({ plugins: [cssnext()] }),
     ],
